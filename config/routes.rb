bc90b083a485d450eb50a9bf59a278c56cc7dee2
gusmattos1 <gusmattos@gmail.com>
2018-04-20 20:50:26 -0400

    create user model and conroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   root 'pictures#index'
-  get 'pictures' => 'pictures#index'
 
-  post 'pictures' => 'pictures#create'
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id/edit' => "pictures#edit"
-  patch 'pictures/:id' => "pictures#update"
-
-  get 'pictures/:id' => 'pictures#show'
-
-  delete 'pictures/:id' => 'pictures#destroy'
-  
+  resource :sessions, only: [:new, :create, :destroy]
+  resource :users, only: [:new, :create]
+  root 'pictures#index'
+  resources :pictures
 end
